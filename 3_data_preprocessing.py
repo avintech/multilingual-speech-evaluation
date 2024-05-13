5c577ee97600e173e737f68dbef32e0e17eec9cf
samuelgjy <samuelgjy97@gmail.com>
2024-05-13 13:35:38 +0800

    update 3_data_preprocessing

--- 3_data_preprocessing.py
+++ 3_data_preprocessing.py
@@ -4,53 +4,61 @@ import modules.prepare_data as prepare_data
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-df = pd.read_pickle("data/pickles/augmented_data.pkl")
+#df = pd.read_pickle("data/pickles/augmented_data.pkl")
 
 #Feature Extraction
+# try:
+#     for index, row in df.iterrows():
+#         try:
+#             data = prepare_data.load_audio("可 可以帮助到别人 我很骄傲","chinese_b2/4aebaa07-7b48-74fd-9807-748080e801c3_Text_011_Line_11.wav")
+#             #df.at[index,'wav_file_path'] = data['wav_file_path']
+#             df.at[index,'pronunciation_accuracy'] = data['pronunciation_accuracy']
+#             df.at[index,'speech_rate'] = data['speech_rate']
+#             df.at[index,'pause_rate'] = data['pause_rate']
+#             #df.at[index,'mfcc'] = data['mfcc']
+#             #df.at[index,'mean_pitch'] = data['mean_pitch']
+#             #df.at[index,'pitch_range'] = data['pitch_range']
+#             #df.at[index,'std_pitch'] = data['std_pitch']
+#         except Exception as ex:
+#             print(row['audio_path'])
+#             print(ex)
+#         finally:
+#             print(df.iloc[index])
+#             print("-" * 20)
+# except Exception as ex:
+#     print(ex)
+# finally:
+#     df.to_pickle("data/pickles/preprocessing_unmapped_data.pkl")
+
 try:
-    for index, row in df.iterrows():
-        try:
-            data = prepare_data.load_audio(row['text'],row['audio_path'])
-            #df.at[index,'wav_file_path'] = data['wav_file_path']
-            df.at[index,'pronunciation_accuracy'] = data['pronunciation_accuracy']
-            df.at[index,'speech_rate'] = data['speech_rate']
-            df.at[index,'pause_rate'] = data['pause_rate']
-            #df.at[index,'mfcc'] = data['mfcc']
-            #df.at[index,'mean_pitch'] = data['mean_pitch']
-            #df.at[index,'pitch_range'] = data['pitch_range']
-            #df.at[index,'std_pitch'] = data['std_pitch']
-        except Exception as ex:
-            print(row['audio_path'])
-            print(ex)
-        finally:
-            print(df.iloc[index])
-            print("-" * 20)
+    data = prepare_data.load_audio("可以帮助到别人我很骄傲","chinese_b2/4aebaa07-7b48-74fd-9807-748080e801c3_Text_011_Line_11.wav")
 except Exception as ex:
     print(ex)
 finally:
-    df.to_pickle("data/pickles/preprocessing_unmapped_data.pkl")
+    print(data)
+    print
 
-df = df.dropna(subset=['speech_rate','pause_rate'])
+# df = df.dropna(subset=['speech_rate','pause_rate'])
 
-#Normalise data
-columns_to_normalize = ['speech_rate','pause_rate','pronunciation_accuracy']
-scaler = MinMaxScaler()
-scaler.fit(df[columns_to_normalize])
-df[columns_to_normalize] = scaler.transform(df[columns_to_normalize])
-joblib.dump(scaler, 'models/scaler.joblib') #Save Scaler
+# #Normalise data
+# columns_to_normalize = ['speech_rate','pause_rate','pronunciation_accuracy']
+# scaler = MinMaxScaler()
+# scaler.fit(df[columns_to_normalize])
+# df[columns_to_normalize] = scaler.transform(df[columns_to_normalize])
+# joblib.dump(scaler, 'models/scaler.joblib') #Save Scaler
 
-#Map fluency values to based on rubrics provided
-def map_fluency(value):
-    if 0 <= value <= 3:
-        return 0
-    elif 4 <= value <= 5:
-        return 1
-    elif 6 <= value <= 7:
-        return 2
-    elif 8 <= value <= 10:
-        return 3
-    else:
-        return None
-df['fluency'] = df['fluency'].apply(map_fluency)
+# #Map fluency values to based on rubrics provided
+# def map_fluency(value):
+#     if 0 <= value <= 3:
+#         return 0
+#     elif 4 <= value <= 5:
+#         return 1
+#     elif 6 <= value <= 7:
+#         return 2
+#     elif 8 <= value <= 10:
+#         return 3
+#     else:
+#         return None
+# df['fluency'] = df['fluency'].apply(map_fluency)
 
-df.to_pickle("data/pickles/preprocessing_mapped_data.pkl")
+# df.to_pickle("data/pickles/preprocessing_mapped_data.pkl")
